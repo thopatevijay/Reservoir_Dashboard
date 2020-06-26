a24128ec67c525e97bbc66cad42291b9dd3f5c46
thopatevijay <thopatevijay@gmail.com>
2020-06-26 10:32:38 +0530

    Creating Server

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+// Express Framework
 const express = require('express');
-const fetch = require('node-fetch');
-const jsdom = require("jsdom");
 
+// for fetching fetch function
+const fetch = require('node-fetch');
+
+// For passing fetched html result through DOM
+const jsdom = require("jsdom");
 
 const app = express();
 
 app.use(express.json());
-
-
 app.use(express.static('public'));
+
 app.set('views','./src/views');
 app.set('view engine','ejs');
 
+
+// defining PORT
 app.listen(3030, () => console.log('listening at 3030'));
